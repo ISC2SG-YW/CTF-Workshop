26199c49494c4b6b69fa7b08d81de2780d9da7d4
gatari <zavierspamstuff@gmail.com>
2023-12-14 13:40:49 +0800

    made the challenge a bit less guessy

--- extra_chals/traveller/chal/app.py
+++ extra_chals/traveller/chal/app.py
@@ -1,14 +1,22 @@
 from flask import Flask, request
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return open('index.html').read()
 
+@app.route('/pwd')
+def pwd():
+    return os.getcwd()
+
 @app.route('/file')
 def file():
     path = request.args.get('path')
+    print(path)
     if path:
+        if "/" in path or ".." in path:
+            return "Error: Hey, no cheating."
         try:
             with open(path, 'r') as f:
                 file_contents = f.read()
